a4b0e5b28fdacfd88887af92a4d7a1ffa68afdff
zolkins <tester.zolkin@gmail.com>
2022-10-09 18:28:02 +0300

    test commit

--- bus.gd
+++ bus.gd
@@ -7,8 +7,3 @@ func _ready():
 #Если кто-то сталкивается выключить ему отоброжение
 func _on_Area2D_body_entered(body):
 	body.visible = false
-
-# Через 11 секунд переместиться влево
-func _on_Timer_timeout():
-	self.position.x -= 1000
-
