408218360dd0016687dcf2b2400e47090ffbb86f
zolkins <tester.zolkin@gmail.com>
2022-10-09 22:53:06 +0300

    add stop bus

--- Person.gd
+++ Person.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-export (int) var speed = 400
+export (int) var speed = 300
 
 var velocity = Vector2()
-
+# Ходьба
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
@@ -16,8 +16,19 @@ func get_input():
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
+# Анимация
+onready var _animated_sprite = $AnimatedSprite
+func _process(_delta):
+	if velocity.length() != 0:
+		_animated_sprite.play("run")
+	else:
+		_animated_sprite.stop()
+
+# Перемещение
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
-# Если столкновение с автобусом, убрать видимость
+
+	
+
 
--- bus.gd
+++ bus.gd
@@ -1,9 +1,50 @@
 extends KinematicBody2D
 
+export (int) var speed = 800
 
-func _ready():
+var velocity = Vector2()
+
+# Автобус двигается до координаты x
+var i = 0
+
+#func _ready():
+	# Начальная позиция автобуса
+	#position = Vector2(1522, 40)
+	#pass
+
+# Начало движения
+func move():
+	velocity.x = -speed
+
+func _physics_process(delta):
+	if position.x >= -4331.0:
+		move()
+	else:
+		_on_Bus_stop()
+	position += velocity * delta
+
+
+func _on_Bus_stop():
+	# Остановка автобуса
+	velocity.x = 0
 	pass
 
-#Если кто-то сталкивается выключить ему отоброжение
-func _on_Area2D_body_entered(body):
-	body.visible = false
+func _on_Bus_start():
+	# Запуск автобуса
+	velocity.x = speed
+	pass
+
+## Анимация
+#onready var _animated_sprite = $AnimatedSprite
+#func _process(_delta):
+#	if velocity.length() != 0:
+#		_animated_sprite.play("run")
+#	else:
+#		_animated_sprite.stop()
+
+
+
+# Если столкновение с автобусом, убрать видимость
+	
+		
+
