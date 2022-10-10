214efc8f7ddd4b4f5c2274d9ea73574a5842b728
zolkins <tester.zolkin@gmail.com>
2022-10-10 22:11:37 +0300

    add assets + animation to Person.gd

--- Person.gd
+++ Person.gd
@@ -1,34 +1,49 @@
 extends KinematicBody2D
 
 export (int) var speed = 300
-
+onready var _animated_sprite = $animated_sprite
 var velocity = Vector2()
+
 # Ходьба
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
+		_animated_sprite.play("walk_right")
 	if Input.is_action_pressed("left"):
+		_animated_sprite.play("walk_left")
 		velocity.x -= 1
 	if Input.is_action_pressed("down"):
 		velocity.y += 1
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
-	velocity = velocity.normalized() * speed
+	if velocity.length() == 0:
+		_animated_sprite.play("idle")
+	else:
+		velocity = velocity.normalized() * speed
+	move_and_slide(velocity)
 
 # Анимация
-onready var _animated_sprite = $AnimatedSprite
 func _process(_delta):
-	if velocity.length() != 0:
-		_animated_sprite.play("run")
-	else:
-		_animated_sprite.stop()
+	# Анимация ходьбы в разные стороны если нажата клавиша ходьбы
+#	if Input.is_action_pressed("right"):
+#		_animated_sprite.play("right")
+#		print("БЛЯЯЯЯЯЯЯЯТЬ")
 
-# Перемещение
-func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity)
-
-	
-
+#	# Если x = 0 и y = 0 играть анимацию idle
+#	if (velocity.x + velocity.y) == 0:
+#		_animated_sprite.play("idle")
+## Если x > 1 играть анимацию walk_left
+#	if velocity.x < 1:
+#		_animated_sprite.play("walk_left")
+## Если x < 1 играть анимацию walk_right
+#	if velocity.x > 1:
+#		_animated_sprite.play("walk_right")
+### Если y > 1 играть анимацию walk_down
+##	if velocity.y > 1:
+##		_animated_sprite.play("walk_down")
+### Если y < 1 играть анимацию walk_up
+##	if velocity.y < 1:
+##		_animated_sprite.play("walk_up")
 
