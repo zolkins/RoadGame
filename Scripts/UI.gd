e3613eb2bc4e5a600dcf9f99d41f4fe5b28e248f
zolkins <tester.zolkin@gmail.com>
2022-10-16 15:15:25 +0300

    add a timer and death if timeout

---
+++ Scripts/UI.gd
@@ -0,0 +1,47 @@
+extends CanvasLayer
+
+var time:int = 10;
+
+
+	
+	
+func _ready():
+	$Control/Timer.start(1)
+	show_time()
+	
+
+func show_time() -> void:
+	var time_txt
+	
+#	$hud/background.modulate = Color(1, 1, 1, 1)
+#	match time:
+#		3: $hud/background.modulate = Color(1, 1, 0, 1) # jaune
+#		2: $hud/background.modulate = Color(1, .75, 0, 1) # orange clair
+#		1: $hud/background.modulate = Color(1, .50, 0, 1) # orange foncé
+#		0: $hud/background.modulate = Color(1, .30, 0, 1)  # rouge
+
+	if time < 10:
+		time_txt = "Time: 0" + str(time)
+	else:
+		time_txt = "Time: " + str(time)
+
+	$Control/Time.text = time_txt
+
+# Called when the node enters the scene tree for the first time.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	if time == 0 or Global.death == true:
+		Global.death = true
+		yield(get_tree().create_timer(3.0), "timeout")
+		Global.death = false
+		get_tree().reload_current_scene()
+	time -= 1
+
+	show_time()
