7d1f2092fbca1442853ebabfbb58e97c6276632a
zolkins <zolkins@>
2022-10-16 23:14:45 +0300

    upd

--- Scripts/bus.gd
+++ Scripts/bus.gd
@@ -3,11 +3,13 @@ extends KinematicBody2D
 export (int) var speed = 350
 
 var velocity = Vector2()
+var start = false
 
 # Автобус двигается до координаты x
 var i = 0
 var stop
 func _ready():
+	$AnimatedSprite.play("close")
 	randomize()
 	stop = rand_range(177, 1100)
 	position = Vector2(1522, 63)
@@ -15,7 +17,6 @@ func _ready():
 # Начало движения
 func move():
 	velocity.x = -speed
-#	print(position.x)
 
 func _physics_process(delta):
 	if position.x >= stop:
@@ -30,27 +31,23 @@ func _on_Bus_stop():
 	velocity.x = 0
 	pass
 
-func _on_Bus_start():
-	# Запуск автобуса
-	velocity.x = speed
-	pass
-
-## Анимация
-#onready var _animated_sprite = $AnimatedSprite
-#func _process(_delta):
-#	if velocity.length() != 0:
-#		_animated_sprite.play("run")
-#	else:
-#		_animated_sprite.stop()
-
-#    if body is Player:
-#        $Timer.stop()
-#        velocity = Vector2()
-#        $AnimationPlayer.play("stop")
-#        $AnimationPlayer.connect("animation_finished", self, "move")
-
-
+func _process(delta):
+	if start and position.x > -212:
+		velocity.x = -speed
+		position += velocity * delta
+		if position.x > -212:
+			yield(get_tree().create_timer(3), "timeout")
+			get_tree().reload_current_scene()
+	
 func _on_door_body_entered(body):
 	if body.name == "body":
+		$AnimatedSprite.play("open")
 		Global.score += 1
 		Global.hide = true
+		start = true
+
+
+func _on_door_body_exited(body):
+	if body.name == "body":
+		$AnimatedSprite.play("close")
+		start = true
