bcf2bb2af6e44d13ef457b87413b6af5b49025a4
zolkins <57059372+zolkins@users.noreply.github.com>
2022-10-29 23:56:51 +0300

    reload

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -15,6 +15,7 @@ func _input(event):
 		OS.set_window_fullscreen(false)
 	if Input.is_action_pressed("ui_accept"):
 		OS.set_window_fullscreen(true)
+
 func scene(target: String) -> void:
 	#$Control/AnimationScene.play("Open")
 	#yield($Control/AnimationScene, "animation_finished")
